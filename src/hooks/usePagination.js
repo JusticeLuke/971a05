972c2271965661de2d16ca4e6762d43c083d5356
Lucas Justice <35172162+JusticeLuke@users.noreply.github.com>
2022-07-11 12:40:53 -0400

    Implement logic for pagination hook
    usePagination returns an array of numbers to enable users to page through all blog posts.

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,14 +1,29 @@
 export const DOTS = "...";
+Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
+function usePagination({currentPage,  totalCount,  pageSize}) {
+  let paginationArray = [1];
+  const totalPageCount = Math.ceil(totalCount / pageSize);
 
-function usePagination() {
-  /*
-    Rewrite the logic here to map out the pagination to be displayed
-
-    !!!!!! ATTENTION !!!!!!
-    Please replace this comment here with a description of this hook.
-    
-  */
-  return [1, 2, 3, DOTS, 5];
+  //Case 1: Display range of pages if total number of pages is less than or equal to 5
+  if(totalPageCount <= 5){
+    paginationArray = Array.range(1,totalPageCount+1);
+  }
+  //Case 2: Display pages as 1 2 3 4.. 99 when current page is 3 or less
+  else if(currentPage <= 3){
+    paginationArray = [1,2,3,4,DOTS,totalPageCount];
+  }
+  //Case 3: Display pages as 1...6 7 8 9 when current page is less than or
+  //equal to 3 of the total pages
+  else if (currentPage >= totalPageCount-2){
+    let endSequenceArray = Array.range(currentPage-1,totalPageCount+1);
+    paginationArray = [1,DOTS].concat(endSequenceArray);
+  }
+  //Case 4: Display pages as 1...4 5 6...99 where 5 is the current page
+  else{
+    paginationArray = [1,DOTS,currentPage-1,currentPage,currentPage+1,DOTS,totalPageCount];
+  }
+  
+return paginationArray;  
 }
 
 export default usePagination;
